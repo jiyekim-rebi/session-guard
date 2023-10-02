55eac384db0f3a5b7dd88713e810f37d2b1ac1e3
rebia <rebianne03@naver.com>
2023-10-02 17:23:04 +0900

    2023.10.02 session-guard:api - init

---
+++ rest-api/src/test/java/com/session_guard/api/RestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.session_guard.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
