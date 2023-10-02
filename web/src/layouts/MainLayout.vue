47ef3fa7784df2e8ef5230dc319eac9b0be9ce79
rebia <rebianne03@naver.com>
2023-10-02 16:51:08 +0900

    2023.10.02 session-guard - pinia

--- web/src/layouts/MainLayout.vue
+++ web/src/layouts/MainLayout.vue
@@ -18,28 +18,57 @@
       <!--      </q-tabs>-->
     </q-header>
 
-    <q-page-container class="fit column wrap justify-center items-center content-center">
+    <q-page-container
+      v-if="!isLogin"
+      class="fit column wrap justify-center items-center content-center">
+        <div>
+          <q-card class="q-pa-md align-center" style="min-width: 400px; max-width: 400px;">
+            <q-card-section>
+              <h3 class="text-h5">Todo-List 로그인</h3>
+            </q-card-section>
+            <q-card-section>
+              <q-input v-model="userId" label="이메일"/>
+              <q-input v-model="password" label="비밀번호" type="password"/>
+            </q-card-section>
+            <q-card-actions>
+              <q-btn color="primary" @click="requestLogin">로그인</q-btn>
+            </q-card-actions>
+          </q-card>
+        </div>
       <div style="overflow: auto; height: 150px;"></div>
-      <div>
-        <q-card class="q-pa-md align-center" style="min-width: 400px; max-width: 400px;">
-          <q-card-section>
-            <h3 class="text-h5">Todo-List 로그인</h3>
-          </q-card-section>
-          <q-card-section>
-            <q-input label="이메일"/>
-            <q-input label="비밀번호" type="password"/>
-          </q-card-section>
-          <q-card-actions>
-            <q-btn color="primary">로그인</q-btn>
-          </q-card-actions>
-        </q-card>
-      </div>
-<!--      <router-view/>-->
     </q-page-container>
 
+    <q-page-container v-if="isLogin">
+      <router-view/>
+    </q-page-container>
   </q-layout>
 </template>
 
 <script setup>
+import {useLoginStore} from "stores/login"
+import {computed, ref} from "vue";
+
+const login = useLoginStore()
+
+const userId = ref('')
+const password = ref('')
+const isLogin = computed(() => {
+  return login.isLogin
+})
+
+function requestLogin() {
+
+  if (userId.value == '') {
+    alert('ID를 입력해주세요')
+    return
+  }
+
+  if (password.value == '') {
+    alert('Password를 입력해주세요')
+    return
+  }
+
+  login.login(userId.value, password.value)
+}
 
 </script>
